9b6dab8dd9a03f83b1fd82e35146e0a71a45fc4b
alimuhammed90 <unlumuhammedali90@gmail.com>
2026-08-27 21:56:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,45 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Giriş yapıldı:  {bot.user.name}')  # Botun adını konsola çıktı olarak verir
 
+
+# "Gamer" sınıfının tanımı
+# Görev 2. nickname alanını ekleyin
+# Görev 3. email alanını ekleyin
+class Gamer:
+    def __init__(self, name, age, nickname, email):
+        self.name = name
+        self.age = age
+
+        self.nickname = nickname
+        self.email = email
+
+
+
+
+
+        self.games = []
+
+    def add_game(self, game):
+        self.games.append(game)
+
+    # Görev 4: Mesajı değiştirin
+    def introduce(self):
+       print(f"Merhaba, benim adım {self.name}, ve ben {self.age} yaşındayım. Bana her zaman {self.email} adresinden e-posta göndererek ulaşabilirsiniz. Oyunda {self.nickname} takma adıyla beni arayın.")
+
+
+# Oyuncu sınıfının bir örneğini oluşturma
+gamer1 = Gamer("Emre", 14, "PrensEmre", "emre@gmail.com")
+
+# Oyunları ekleme
+gamer1.add_game("Minecraft")
+gamer1.add_game("Dota 2")
+
+# Bir oyuncunun mini sunumu
+gamer1.introduce()
+
+# Favori oyunların çıktısının alınması
+print(f"{gamer1.name} şu oyunları seviyor: {', '.join(gamer1.games)}")
+
 # '!go' komutu
 @bot.command()
 async def go(ctx):
