138b2cf1c48cfa4b4c39608bf32d37b706aedfaa
alimuhammed90 <unlumuhammedali90@gmail.com>
2026-09-03 22:20:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,197 @@
+
 import discord
 from discord.ext import commands
+
 from config import token
 from logic import Pokemon
 
-# Bot için yetkileri/intents ayarlama
-intents = discord.Intents.default()  # Varsayılan ayarların alınması
-intents.messages = True              # Botun mesajları işlemesine izin verme
-intents.message_content = True       # Botun mesaj içeriğini okumasına izin verme
-intents.guilds = True                # Botun sunucularla çalışmasına izin verme
 
-# Tanımlanmış bir komut önekine ve etkinleştirilmiş amaçlara sahip bir bot oluşturma
-bot = commands.Bot(command_prefix='!', intents=intents)
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.messages = True
 
-# Bot çalışmaya hazır olduğunda tetiklenen bir olay
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+
 @bot.event
 async def on_ready():
-    print(f'Giriş yapıldı:  {bot.user.name}')  # Botun adını konsola çıktı olarak verir
+    print(f"Bot giriş yaptı: {bot.user}")
+    print("Komutlar hazır!")
 
 
-# "Gamer" sınıfının tanımı
-# Görev 2. nickname alanını ekleyin
-# Görev 3. email alanını ekleyin
-class Gamer:
-    def __init__(self, name, age, nickname, email):
-        self.name = name
-        self.age = age
-
-        self.nickname = nickname
-        self.email = email
-
-
-
-
-
-        self.games = []
-
-    def add_game(self, game):
-        self.games.append(game)
-
-    # Görev 4: Mesajı değiştirin
-    def introduce(self):
-       print(f"Merhaba, benim adım {self.name}, ve ben {self.age} yaşındayım. Bana her zaman {self.email} adresinden e-posta göndererek ulaşabilirsiniz. Oyunda {self.nickname} takma adıyla beni arayın.")
-
-
-# Oyuncu sınıfının bir örneğini oluşturma
-gamer1 = Gamer("Emre", 14, "PrensEmre", "emre@gmail.com")
-
-# Oyunları ekleme
-gamer1.add_game("Minecraft")
-gamer1.add_game("Dota 2")
-
-# Bir oyuncunun mini sunumu
-gamer1.introduce()
-
-# Favori oyunların çıktısının alınması
-print(f"{gamer1.name} şu oyunları seviyor: {', '.join(gamer1.games)}")
-
-# '!go' komutu
 @bot.command()
 async def go(ctx):
-    author = ctx.author.name  # Mesaj yazarının adını alma
-    # Kullanıcının zaten bir Pokémon'u olup olmadığını kontrol edin. Eğer yoksa, o zaman...
-    if author not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(author)  # Yeni bir Pokémon oluşturma
-        await ctx.send(await pokemon.info())  # Pokémon hakkında bilgi gönderilmesi
-        image_url = await pokemon.show_img()  # Pokémon resminin URL'sini alma
-        if image_url:
-            embed = discord.Embed()  # Gömülü mesajı oluşturma
-            embed.set_image(url=image_url)  # Pokémon'un görüntüsünün ayarlanması
-            await ctx.send(embed=embed)  # Görüntü içeren gömülü bir mesaj gönderme
-        else:
-            await ctx.send("Pokémonun görüntüsü yüklenemedi!")
+
+    author = str(ctx.author.id)
+
+    if author in Pokemon.pokemons:
+        await ctx.send(
+            "Zaten bir Pokémon'un var!\n"
+            "Bilgilerini görmek için `!pokemon` yaz."
+        )
+        return
+
+    pokemon = Pokemon(author)
+
+    info = await pokemon.info()
+
+    await ctx.send(
+        f"Pokémon'un oluşturuldu!\n\n"
+        f"{info}"
+    )
+
+    image_url = await pokemon.show_img()
+
+    if image_url:
+
+        embed = discord.Embed(
+            title=f"{pokemon.name}"
+        )
+
+        embed.set_image(url=image_url)
+
+        await ctx.send(embed=embed)
+
+
+@bot.command()
+async def pokemon(ctx):
+
+    author = str(ctx.author.id)
+
+    if author not in Pokemon.pokemons:
+
+        await ctx.send(
+            "Henüz Pokémon'un yok!\n"
+            "Önce `!go` yaz."
+        )
+        return
+
+    pokemon = Pokemon.pokemons[author]
+
+    info = await pokemon.info()
+
+    await ctx.send(info)
+
+
+@bot.command()
+async def info(ctx):
+
+    author = str(ctx.author.id)
+
+    if author in Pokemon.pokemons:
+        pok = Pokemon.pokemons[author]
+        await ctx.send(await pok.info())
     else:
-        await ctx.send("Zaten kendi Pokémonunuzu oluşturdunuz!")  # Bir Pokémon'un daha önce oluşturulup oluşturulmadığını gösteren bir mesaj
-# Botun çalıştırılması
+        await ctx.send(
+            "Önce !go komutunu kullanarak bir Pokémon oluşturmalısın."
+        )
+
+
+@bot.command()
+async def besle(ctx):
+
+    author = str(ctx.author.id)
+
+    if author not in Pokemon.pokemons:
+
+        await ctx.send(
+            "Önce `!go` komutuyla Pokémon oluşturmalısın."
+        )
+        return
+
+    pokemon = Pokemon.pokemons[author]
+
+    result = pokemon.feed()
+
+    await ctx.send(result)
+
+
+@bot.command()
+async def attack(ctx, enemy: discord.Member):
+
+    attacker_id = str(ctx.author.id)
+    enemy_id = str(enemy.id)
+
+    if attacker_id == enemy_id:
+
+        await ctx.send(
+            "Kendine saldıramazsın!"
+        )
+        return
+
+    if attacker_id not in Pokemon.pokemons:
+
+        await ctx.send(
+            "Önce `!go` ile kendi Pokémon'unu oluştur."
+        )
+        return
+
+    if enemy_id not in Pokemon.pokemons:
+
+        await ctx.send(
+            f"{enemy.mention} adlı kullanıcının Pokémon'u yok."
+        )
+        return
+
+    attacker = Pokemon.pokemons[attacker_id]
+    defender = Pokemon.pokemons[enemy_id]
+
+    if attacker.hp <= 0:
+
+        await ctx.send(
+            "Pokémon'unun HP'si 0!\n"
+            "Savaşamazsın."
+        )
+        return
+
+    if defender.hp <= 0:
+
+        await ctx.send(
+            "Bu Pokémon'un HP'si zaten 0."
+        )
+        return
+
+    result = await attacker.attack(defender)
+
+    await ctx.send(result)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+
+    if isinstance(error, commands.CommandNotFound):
+
+        await ctx.send(
+            "Böyle bir komut yok.\n"
+            "Kullanabileceğin komutlar:\n"
+            "`!go`\n"
+            "`!pokemon`\n"
+            "`!info`\n"
+            "`!besle`\n"
+            "`!attack @kullanıcı`"
+        )
+
+    elif isinstance(error, commands.MissingRequiredArgument):
+
+        await ctx.send(
+            "Eksik bilgi.\n"
+            "Örnek: `!attack @kullanıcı`"
+        )
+
+    elif isinstance(error, commands.MemberNotFound):
+
+        await ctx.send(
+            "Kullanıcı bulunamadı."
+        )
+
+    else:
+
+        print(f"Hata: {error}")
+
+
 bot.run(token)
+
